828526d32682eff044315eee704adcc00e236ee7
DarthWirthe <32981484+DarthWirthe@users.noreply.github.com>
2020-11-22 15:29:25 +0400

    Update scr_vm.h

--- src/scr_vm.h
+++ src/scr_vm.h
@@ -1,36 +1,27 @@
 
 /*
-	Виртуальная машина, заголовочный файл.
+	Заголовочный файл.
+	Виртуальная машина.
 */
 
 #pragma once
 #ifndef SCR_VM_H
 #define SCR_VM_H
 
-#include "scr_parser.h"
-#include "scr_tref.h"
 #include "scr_types.h"
 #include "scr_heap.h"
-//#include "scr_stack.h"
 #include "scr_opcodes.h"
-//#include "utils/scr_dbg.h"
 
 #define FRAME_REQUIREMENTS 3
-
-stack_t float_to_stack(float val);
-float stack_to_float(stack_t val);
-int_t uint_to_int32(uint32_t val);
-short_t uint_to_short16(uint16_t val);
 
 class VM
 {
 	public:
-		VM(Parser obj);
-		void Execute(void);
-		void Assembly(void);
-		void Interpret(void);
-		Parser _parser_obj;
-		
+		VM(void);
+		VM(uint16_t h_size, uint8_t *code, num_t *c);
+		void Run(void);
+	private:
+		Heap *heap;
 		uint8_t *code_base;
 		uint8_t *stack_base;
 		num_t *consts;
