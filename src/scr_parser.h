91d61f6beded27fd2f7723a8ac095068fbe1dc0d
DarthWirthe <32981484+DarthWirthe@users.noreply.github.com>
2020-11-22 15:32:35 +0400

    Update scr_parser.h

--- src/scr_parser.h
+++ src/scr_parser.h
@@ -1,46 +1,61 @@
 
+/* scr_parser.h
+** Заголовочный файл.
+** Синтаксический анализатор (парсер)
+*/
+
 #pragma once
 #ifndef SCR_PARSER_H
 #define SCR_PARSER_H
 
+#include <string>
+#include <vector>
 #include "scr_lex.h"
 #include "scr_code.h"
-#include <string.h>
-//#include "utils/scr_dbg.h"
 
-struct node_stack {
-	node node_stack[16];
-	int nst_pt = -1;
-	void Push(node n);
-	node Pop(void);
-	node Top(void);
+struct var_reg {
+	char *name;
+	VarType type; // из code.h
+};
+
+static const var_reg var_types_list[] = {
+	{(char*)"int", VarType::Int},
+	{(char*)"float", VarType::Float}
 };
 
 class Parser
 {
 	public:
 		Parser(void);
-		Parser(Lexer _lexer_obj);
-		void Exception(void);
+		Parser(Lexer *_lexer_class);
+		void Exception(std::string s);
+		void ErrorExpectedToken(Token::Type expected, Token::Type got);
 		void Next(Token::Type);
+		void NewLine(void);
 		void StatementList(void);
 		void Statement(void);
+		void Assignment(char *name);
+		void Declaration(void);
+		uint8_t FunctionArgs(void);
 		void FunctionDeclaration(void);
 		void FunctionCall(char *name);
-		void Assignment(char *name);
+		void ReturnStatement(void);
 		void ArrayDeclaration(char *name);
-		void IfStatement(uint8_t *ifs_pt);
-		void ElseStatement(uint8_t *ifs_pt);
+		void IfStatement(void);
 		void WhileStatement(void);
-		void Expr(uint8_t lp);
+		std::vector<Node> Expr(uint8_t lp);
+		std::vector<Node> ConstExpr(uint8_t lp);
 		void Program(void);
-		void Parse(void);	
+		void Parse(void);
 		uint8_t* GetCode(void);
 		num_t* GetConsts(void);
-		int GetConstsCount(void);
+		size_t GetCodeLength(void);
+		size_t GetConstsCount(void);
 		
-		Code coder_obj;
-		Lexer lexer_obj;
+		bool Terminated;
+	private:
+		Code *code;
+		Lexer *lex;
 		Token current_token;
 };
 
