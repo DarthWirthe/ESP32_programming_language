ce977923bb9274b3fa1cb910828df3c2b9914b04
DarthWirthe <32981484+DarthWirthe@users.noreply.github.com>
2020-11-22 15:33:02 +0400

    Update scr_heap.h

--- src/scr_heap.h
+++ src/scr_heap.h
@@ -1,26 +1,43 @@
+/*
+** 
+*/
 
 #pragma once
 #ifndef SCR_HEAP_H
 #define SCR_HEAP_H
 
-#include <stdint.h>
+#include "scr_types.h"
 
-#define DEFAULT_HEAP_SIZE 256
 #define HEAP_ID_FREE 0
 
 typedef uint16_t heap_id_t;
 
-void heap_dbg(void);
-heap_id_t heap_alloc(uint16_t size);
-void heap_free(heap_id_t id);
-void heap_realloc(heap_id_t id, uint16_t size);
-uint16_t heap_get_length(heap_id_t id);
-void* heap_get_addr(heap_id_t id);
-void heap_init(void);
-uint16_t heap_get_free_size(void);
-uint8_t* heap_get_base(void);
-uint8_t* heap_get_relative_base(void);
-void heap_steal(uint16_t length);
-void heap_unsteal(uint16_t length);
+// Заголовок блока
+typedef struct {
+	heap_id_t id; // 2 б
+	uint16_t len; // 2 б
+} heaphdr_t;
+
+class Heap {
+	private:
+		heap_t *heap;
+		uint16_t base;
+		uint16_t length;
+	public:
+		Heap(int _length);
+		uint16_t get_base(void);
+		heap_t* get_heap(void);
+		uint16_t get_length(void);
+		void heap_dmp(void);
+		heap_id_t alloc(uint16_t size);
+		void free(heap_id_t id);
+		void realloc(heap_id_t id, uint16_t size);
+		uint16_t get_length(heap_id_t id);
+		void* get_addr(heap_id_t id);
+		uint16_t get_free_size(void);
+		heap_t* get_relative_base(void);
+		void mem_steal(uint16_t length);
+		void mem_unsteal(uint16_t length);
+};
 
 #endif
