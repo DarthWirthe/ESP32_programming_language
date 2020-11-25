c6ef76504b1f32bdc17d008ac576734f56b40ebb
DarthWirthe <32981484+DarthWirthe@users.noreply.github.com>
2020-11-25 12:36:31 +0400

    Update scr_tref.h

--- src/scr_tref.h
+++ src/scr_tref.h
@@ -10,9 +10,10 @@ bool Compare_strings(char *s1, char *s2);
 bool Compare_strings(const char s1[], char *s2);
 bool Compare_strings(char *s1, const char s2[]);
 bool Compare_strings(const char s1[], const char s2[]);
-int String_to_int(char*s);
-float String_to_float(char *s);
+int Const_char_to_int(const char*s);
+float Const_char_to_float(const char *s);
 void Copy_str(char *s1, char *s2);
+void Copy_str(const char *s1, char *s2);
 
 
 #endif
