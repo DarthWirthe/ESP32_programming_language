96addd6791876a8e82decdf69fc1bb38dad57345
DarthWirthe <32981484+DarthWirthe@users.noreply.github.com>
2020-12-20 15:49:51 +0400

    Update scr_parser.h

--- src/scr_parser.h
+++ src/scr_parser.h
@@ -8,51 +8,45 @@
 #ifndef SCR_PARSER_H
 #define SCR_PARSER_H
 
-#include <string>
 #include <vector>
 #include "scr_lex.h"
 #include "scr_code.h"
-
-struct var_reg {
-	char *name;
-	VarType type; // из code.h
-};
-
-static const var_reg var_types_list[] = {
-	{(char*)"int", VarType::Int},
-	{(char*)"float", VarType::Float}
-};
 
 class Parser
 {
 	public:
 		Parser(void);
 		Parser(Lexer *_lexer_class);
-		void Exception(std::string s);
+		void InternalException(int code);
 		void ErrorExpectedToken(Token::Type expected, Token::Type got);
+		void ErrorExpectedExpr(void);
+		void ErrorExpectedSymbol(const char *c);
+		void ErrorExtraSymbol(const char *c);
 		void Next(Token::Type);
 		void NewLine(void);
 		void StatementList(void);
 		void Statement(void);
 		void Assignment(char *name);
-		void Declaration(void);
-		uint8_t FunctionArgs(void);
+		VarType VariableType(void);
+		void Declaration(char *name, VarType t);
+		std::vector<func_arg_struct> FunctionArgs(void);
 		void FunctionDeclaration(void);
 		void FunctionCall(char *name);
 		void ReturnStatement(void);
-		void ArrayDeclaration(char *name);
+		void ArrayDeclaration(char *name, VarType t);
 		void IfStatement(void);
 		void WhileStatement(void);
+		char* ParseIdentSequence(char *first_val);
 		std::vector<Node> Expr(uint8_t lp);
-		std::vector<Node> ConstExpr(uint8_t lp);
 		void Program(void);
-		void Parse(void);
+		int Parse(void);
+		Code* Compiler(void);
 		uint8_t* GetCode(void);
 		num_t* GetConsts(void);
 		size_t GetCodeLength(void);
 		size_t GetConstsCount(void);
 		
-		bool Terminated;
+		bool exit_code;
 	private:
 		Code *code;
 		Lexer *lex;
