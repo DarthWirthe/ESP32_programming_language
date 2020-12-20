cbd7fa378857414d4d98aee41aab35947197905f
DarthWirthe <32981484+DarthWirthe@users.noreply.github.com>
2020-12-20 15:50:24 +0400

    Update scr_types.h

--- src/scr_types.h
+++ src/scr_types.h
@@ -5,23 +5,6 @@
 
 #include <stdint.h> // для фиксированных типов
 
-#define IMMEDIATE_MASK_16 0x8000 // маска для знаковых типов 16 бит
-#define IMMEDIATE_MASK_32 0x80000000 // маска для знаковых типов 32 бит
-
-typedef int8_t  char_t;
-typedef int16_t short_t;
-typedef int32_t int_t;
-
-typedef uint8_t  heap_t;
-typedef uint32_t stack_t;
-
-typedef union {
-  char  b[4];
-  short s[2];
-  int   i[1];
-  float f[1];
-} union_t;
-
 typedef union {
   int   i;
   float f;
