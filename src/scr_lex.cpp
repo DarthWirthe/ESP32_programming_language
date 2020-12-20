d53a1a18f75484efa6159075365b2a770e59ccec
DarthWirthe <32981484+DarthWirthe@users.noreply.github.com>
2020-12-20 15:50:46 +0400

    Update scr_lex.cpp

--- src/scr_lex.cpp
+++ src/scr_lex.cpp
@@ -1,21 +1,27 @@
 
+// Лексический анализатор
+
+
 #include "scr_lex.h"
 #include "HardwareSerial.h"
 
-char LEX_C_BUFFER[C_BUFFER_LEN];
 
 const char* token_name[] {
-	"If", "Elseif", "Else", "While", "Do", "For", "Function", "Return", "LeftParen", "RightParen","LeftSquare", "RightSquare", "LeftCurly", "RightCurly", 
-	"Assign", "Less", "LessOrEqual", "Equal", "NotEqual", "Greater", "GreaterOrEqual", "Plus", "Minus", "Asterisk", "Slash", "Modulo", "And", "Or", "Not",
-	"Dot", "Comma", "Colon", "Semicolon", "SingleQuote", "DoubleQuote", "Int", "Float",
-	"Identifier", "NewLine", "End", "Unexpected"
+	"If", "Elseif", "Else", "While", "Do", "For", "Function", "Return", "And",
+	"Or", "Const", "Global", "LeftParen", "RightParen", "LeftSquare",
+	"RightSquare", "LeftCurly", "RightCurly", "Assign", "Less",
+	"LessOrEqual", "Equal", "NotEqual", "Greater", "GreaterOrEqual",
+	"DoubleLess", "DoubleGreater", "Plus", "Minus", "Asterisk", "Slash",
+	"Modulo", "Not", "Tilde", "Ampersand", "Pipe", "Dot", "Comma", "Colon",
+	"Semicolon", "SingleQuote", "DoubleQuote", "Int", "Float", "String",
+	"Identifier", "NewLine", "EOF", "Unexpected"
 };
 
 const char* Token_to_string(Token::Type t) {
 	return token_name[(int)t];
 }
 
-static const char* reserved_keywords[] = {
+const char* reserved_keywords[] = {
 	"if",
 	"elseif",
 	"else",
@@ -25,12 +31,12 @@ static const char* reserved_keywords[] = {
 	"function",
 	"return",
 	"and",
-	"or"
+	"or",
+	"const",
+	"global"
 };
 
-Token::Token() {
-	
-}
+Token::Token() {}
 
 Token::Token(Type _type) {
 	type = _type;
@@ -47,25 +53,26 @@ Token::Token(Type _type, char* _value){ // тип и значение
 	value.c = _value;
 }
 
-bool IsLetterChar(char c){
-	if ( ( c >= 'A' && c <= 'z' ) || c == '_' )		
+static bool IsLetterChar(char c){
+	if (( c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z' ) || c == '_')		
 		return true;
 	return false;
 }
 
-bool IsDigit(char c){
+static bool IsDigit(char c){
 	if ( c >= '0' && c <= '9' )
 		return true;
 	return false;
 }
 
-bool IsIdentifiedChar(char c){
+static bool IsIdentifiedChar(char c){
 	if ( IsLetterChar(c) || IsDigit(c) )
 		return true;
 	return false;
 }
 
-bool IsSpace(char c){ // является ли символ пробелом
+// является ли символ пробелом
+static bool IsSpace(char c) { 
 	switch (c) {
 		case ' ':
 		case '\t':
@@ -76,22 +83,16 @@ bool IsSpace(char c){ // является ли символ пробелом
 	}
 }
 
-int FindReserved(){
+int Lexer::FindReserved() {
 	for(int i = 0; i < RESERVED_NUM; i++)
 	{
-		if(Compare_strings(LEX_C_BUFFER, reserved_keywords[i]))
+		if(Compare_strings(T_BUF.c_str(), reserved_keywords[i]))
 			return i;
 	}
 	return -1;
 }
 
-bool IsReserved(){
-	if(FindReserved() == -1)
-		return false;
-	return true;
-}
-
-Token ReservedToken(){
+Token Lexer::ReservedToken() {
 	return Token((Token::Type)FindReserved());
 }
 
@@ -122,10 +123,11 @@ char Lexer::Peek(){
 	} else if (InputType ==  LEX_INPUT_STRING) {
 		return *pointer;
 	}
+	return 0;
 }
 
 char Lexer::PeekNext(){
-	return *(pointer+1);
+	return *(pointer + 1);
 }
 
 char Lexer::Get(){
@@ -136,47 +138,50 @@ char Lexer::Get(){
 	} else if (InputType ==  LEX_INPUT_STRING) {
 		return *pointer++;
 	}
+	return 0;
 }
 
 Token Lexer::Next()
 {
 	Token token;
+	token.value.c = nullptr; // !Пометить токен NULLPTR для последующего удаления
 	char last_c;
 	char temp_char;
 	char *cpoint;
-	int len;
-	
-	while (IsSpace(Peek())){Get();column++;} // пропустить пробелы
+
+lexer_start: // 'goto label'
+	last_c = Peek();
+	while (IsSpace(last_c)) { // пропустить пробелы и табы
+		Get();
+		column++;
+		last_c = Peek();
+	}
 
 	last_c = Get();
 	if (IsLetterChar(last_c)) // если найдена буква
 	{
-		len = 0;
 		temp_char = last_c;
 		while (true)
 		{
 			if (IsIdentifiedChar(temp_char))
 			{
-				if (len < (C_BUFFER_LEN - 1))
-				{
-					LEX_C_BUFFER[len] = temp_char;
-					len++;
-					column++;
-				}
+				T_BUF += temp_char;
+				column++;
 			}
 			if (!IsIdentifiedChar(Peek()))
 			{
-				LEX_C_BUFFER[len] = '\0';
-				if (IsReserved())
+				if (FindReserved() != -1) // если это ключевое слово
 				{
 					token = ReservedToken();
+					ClearBuffer();
 					last_token = token;
 					return token;
-				} else {
-					cpoint = new char[len + 1];
-					Copy_str(LEX_C_BUFFER, cpoint);
+				} else { // иначе это идентификатор
+					cpoint = new char[T_BUF.size() + 1];
+					Copy_str(T_BUF.c_str(), cpoint);
 					token.type = Token::Type::Identifier;
 					token.value.c = cpoint;
+					ClearBuffer();
 					last_token = token;
 					return token;
 				}
@@ -187,62 +192,61 @@ Token Lexer::Next()
 	else if (IsDigit(last_c)) // если найдена цифра
 	{
 		bool IsFloat = false;
-		len = 0;
 		temp_char = last_c;
 		while (1)
 		{
 				if (IsDigit(temp_char))
 				{
-					if (len < (C_BUFFER_LEN - 1))
-					{
-						LEX_C_BUFFER[len] = temp_char;
-						len++;
-						column++;
-					}
+					T_BUF += temp_char;
+					column++;
 				}
 				if (Peek() == '.') {
 					if (!IsFloat) {
 						IsFloat = true;
-						LEX_C_BUFFER[len] = '.';
-						len++;
+						T_BUF += '.';
 						column++;
 					} else {
 						/*Ошибка!*/
 					}
 				} else if (!IsDigit(Peek())) {
-					LEX_C_BUFFER[len] = '\0';
 					if (!IsFloat) {
-						token.value.i = String_to_int(LEX_C_BUFFER);
+						token.value.i = Const_char_to_int(T_BUF.c_str());
 						token.type = Token::Type::Int;
 					} else {
-						token.value.f = String_to_float(LEX_C_BUFFER);
+						token.value.f = Const_char_to_float(T_BUF.c_str());
 						token.type = Token::Type::Float;
 					}
+					ClearBuffer();
 					last_token = token;
 					return token;
 				}
 			temp_char = Get();
 		}
 	}
-	else if (last_c == '@') // Пропускает комментарий до конца строки
+	else if (last_c == '"') // двойная кавычка
 	{
-		while (Peek() != '\0')
-		{
-			column++;
-			if (Get() == '\n')
-			{
+		temp_char = Get(); // текст между кавычек записывается в буфер
+		while (!EndOfFile(temp_char) && temp_char != '"') {
+			if (temp_char == '\n') {
 				column = 1;
 				line++;
-				token.type = Token::Type::NewLine;
-				last_token = token;
-				return token;
+			} else {
+				T_BUF += temp_char;
+				column++;
 			}
+			temp_char = Get();
 		}
+		cpoint = new char[T_BUF.size() + 1];
+		Copy_str(T_BUF.c_str(), cpoint);
+		token.value.c = cpoint;
+		ClearBuffer();
+		token.type = Token::Type::String;
+		last_token = token;
+		return token;
 	}
-	else if (last_c == '\n') // Конец строки
-	{
-		column = 1;
+	else if (last_c == '\n') {
 		line++;
+		column = 1;
 		token.type = Token::Type::NewLine;
 		last_token = token;
 		return token;
@@ -291,8 +295,13 @@ Token Lexer::Next()
 	}
 	else if (last_c == '<') // меньше
 	{
-		if (Peek() == '=') {
+		if (Peek() == '=') { // <=
 			token.type = Token::Type::LessOrEqual;
+			Get();
+			column+=2;
+		}
+		else if (Peek() == '<') { // <<
+			token.type = Token::Type::DoubleLess;
 			Get();
 			column+=2;
 		}
@@ -306,8 +315,13 @@ Token Lexer::Next()
 	}
 	else if (last_c == '>') // больше
 	{
-		if (Peek() == '=') {
+		if (Peek() == '=') { // >=
 			token.type = Token::Type::GreaterOrEqual;
+			Get();
+			column+=2;
+		}
+		else if (Peek() == '>') { // >>
+			token.type = Token::Type::DoubleGreater;
 			Get();
 			column+=2;
 		}
@@ -355,10 +369,35 @@ Token Lexer::Next()
 	}
 	else if (last_c == '/') // косая черта (деление)
 	{
-		column++;
-		token.type = Token::Type::Slash;
-		last_token = token;
-		return token;
+		// Комментарий
+		if (Peek() == '/') {
+			while (!EndOfFile(Peek())) {
+				if (Get() == '\n') {
+					column = 1;
+					line++;
+					goto lexer_start; // комментарий пропущен
+				}
+			}
+		}
+		else if (Peek() == '*') {
+			while (!EndOfFile(Peek())) {
+				last_c = Get();
+				column++;
+				if (last_c == '*' && Peek() == '/') {
+					Get();
+					goto lexer_start; // комментарий пропущен
+				}
+				if (last_c == '\n') {
+					column = 1;
+					line++;
+				}
+			}
+		} else {
+			column++;
+			token.type = Token::Type::Slash;
+			last_token = token;
+			return token;
+		}
 	}
 	else if (last_c == '%') // знак процента (остаток от деления)
 	{
@@ -367,7 +406,7 @@ Token Lexer::Next()
 		last_token = token;
 		return token;
 	}
-	else if (last_c == '!') // восклицательный знак (НЕ)
+	else if (last_c == '!') // восклицательный знак (отрицание)
 	{
 		if (Peek() == '=') {
 			token.type = Token::Type::NotEqual;
@@ -381,20 +420,27 @@ Token Lexer::Next()
 		last_token = token;
 		return token;
 	}
-	// else if (last_c == '&') // И
-	// {
-		// column++;
-		// token.type = Token::Type::And;
-		// last_token = token;
-		// return token;
-	// }
-	// else if (last_c == '|') // ИЛИ
-	// {
-		// column++;
-		// token.type = Token::Type::Or;
-		// last_token = token;
-		// return token;
-	// }
+	else if (last_c == '~') // Тильда (побитовое отрицание)
+	{
+		column++;
+		token.type = Token::Type::Tilde;
+		last_token = token;
+		return token;
+	}
+	else if (last_c == '&') // побитовое И
+	{
+		column++;
+		token.type = Token::Type::Ampersand;
+		last_token = token;
+		return token;
+	}
+	else if (last_c == '|') // побитовое ИЛИ
+	{
+		column++;
+		token.type = Token::Type::Pipe;
+		last_token = token;
+		return token;
+	}
 	else if (last_c == '.') // точка (разделитель числа)
 	{
 		column++;
@@ -430,29 +476,34 @@ Token Lexer::Next()
 		last_token = token;
 		return token;
 	}
-	else if (last_c == '"') // двойная кавычка
+	else if (EndOfFile(last_c)) // Конец ввода
 	{
-		column++;
-		token.type = Token::Type::DoubleQuote;
-		last_token = token;
-		return token;
-	}
-	else if (last_c == '\0') // Конец ввода
-	{
-		token.type = Token::Type::End;
-		last_token = token;
-		return token;
-	}
-	else if(InputType == LEX_INPUT_FILE && !file.available()) {
-		token.type = Token::Type::End;
+		token.type = Token::Type::EndOfFile;
 		last_token = token;
 		return token;
 	}
 	token.type = Token::Type::Unexpected;
+	token.value.i = (uint8_t)last_c;
 	last_token = token;
-	Serial.print("Unexpected symbol #");
-	Serial.println((uint8_t)last_c);
 	return token; // неожиданный символ
+}
+
+void Lexer::ClearBuffer() {
+	T_BUF.clear();
+	T_BUF.shrink_to_fit();
+}
+
+bool Lexer::EndOfFile(char c) {
+	if (c == '\0')
+		return true;
+	if (InputType == LEX_INPUT_FILE && !file.available())
+		return true;
+	return false;
+}
+
+void Lexer::End() {
+	if (InputType == LEX_INPUT_FILE)
+		SPIFFS_CLOSE_FILE(file);
 }
 
 
