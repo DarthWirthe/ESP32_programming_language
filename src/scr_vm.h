d5b84483374c1156bbeb39ff09906163a18a14e1
DarthWirthe <32981484+DarthWirthe@users.noreply.github.com>
2020-12-20 15:52:03 +0400

    Update scr_vm.h

--- src/scr_vm.h
+++ src/scr_vm.h
@@ -1,8 +1,8 @@
 
-/*
-	Заголовочный файл.
-	Виртуальная машина.
-*/
+/**
+*** Заголовочный файл.
+*** Виртуальная машина.
+**/
 
 #pragma once
 #ifndef SCR_VM_H
@@ -12,19 +12,56 @@
 #include "scr_heap.h"
 #include "scr_opcodes.h"
 
-#define FRAME_REQUIREMENTS 3
+#define IMMEDIATE_MASK_16 0x8000 // для знаковых типов 16 бит
+#define IMMEDIATE_MASK_32 0x80000000 // для знаковых типов 32 бит
+
+#define VM_NOERROR 0x00
+#define VM_ERR_UNKNOWN_INSTR 0x01
+#define VM_ERR_EXPECTED_FUNCTION 0x08
+#define VM_ERR_INDEX_OUT_OF_RANGE 0x09
+
+typedef int8_t  char_t;
+typedef int16_t short_t;
+typedef int32_t int_t;
+
+typedef uint32_t stack_t;
+
+typedef union {
+    char  b[4];
+    short s[2];
+    int   i[1];
+    float f[1];
+} union_t;
+
+typedef union {
+    uint16_t U16;
+    uint16_t I16;
+    struct {
+        uint8_t L, H;
+    } U8;
+    int_t I32;
+} arg_t;
+
+#define FRAME_REQUIREMENTS 4
 
 class VM
 {
 	public:
 		VM(void);
-		VM(uint16_t h_size, uint8_t *code, num_t *c);
-		void Run(void);
+		VM(uint16_t h_size, uint8_t *ptcode, num_t *nums, char *strs, int g_count);
+        stack_t LoadString(char *c);
+		int Run(void);
+        void StackPush(stack_t val);
+        stack_t StackPop(void);
+        void CleanMemory(void);
 	private:
 		Heap *heap;
-		uint8_t *code_base;
-		uint8_t *stack_base;
-		num_t *consts;
+		uint8_t *code_base; // указатель на начало кода
+		uint8_t *stack_base; // указатель на начало стека
+		num_t *consts_num_base;
+        char *consts_str_base;
+        stack_t *_sp;
+        int globals_count;
 };
 
-#endif
+#endif // SCR_VM_H
