0f8a87f3711dd2135ef46bbb687ec04bc5cd13dc
DarthWirthe <32981484+DarthWirthe@users.noreply.github.com>
2020-12-20 15:48:09 +0400

    Update scr_heap.h

--- src/scr_heap.h
+++ src/scr_heap.h
@@ -1,5 +1,5 @@
-/*
-** 
+/* scr_heap.h
+** Виртуальная память
 */
 
 #pragma once
@@ -10,34 +10,54 @@
 
 #define HEAP_ID_FREE 0
 
-typedef uint16_t heap_id_t;
+// Коды ошибок
+#define HEAP_ERR_NOT_FOUND 0x20
+#define HEAP_ERR_OUT_OF_MEMORY 0x21
+#define HEAP_ERR_CORRUPTED 0x22
+#define HEAP_ERR_OVERFLOW 0x23
+#define HEAP_ERR_UNDERFLOW 0x24
+
+// Макросы для битовых операций
+#define BITREAD(NUM, BIT) (((NUM) >> (BIT)) & 1)
+#define BITSET(NUM, BIT) ((NUM) |= (1UL << (BIT)))
+#define BITCLEAR(NUM, BIT) ((NUM) &= ~(1UL << (BIT)))
+#define BITWRITE(NUM, BIT, VAL) ((VAL) ? BITSET((NUM), (BIT)) : BITCLEAR((NUM), (BIT)))
+
+// Маска для типа блока - 2 старших бита
+#define HEAP_TYPE_MASK 0xC0000000U
+/* Т.к. ид блока занимает только 14 бит, нужно убирать первые 2*/
+#define HEAP_ID_MASK 0x3FFFU
+
+typedef uint8_t  heap_t;
+typedef uint16_t u16_t;
 
 // Заголовок блока
 typedef struct {
-	heap_id_t id; // 2 б
-	uint16_t len; // 2 б
+	u16_t id;
+	uint16_t len;
 } heaphdr_t;
 
 class Heap {
-	private:
+	public:
+		Heap(int size);
+		void heap_dmp(void);
+		u16_t heap_new_id(void);
+		bool heap_alloc_internal(u16_t id, uint16_t size);
+		u16_t alloc(uint16_t size);
+		void free(u16_t id);
+		void realloc(u16_t id, uint16_t size);
+		uint16_t get_length(u16_t id);
+		void* get_addr(u16_t id);
+		uint16_t get_free_size(void);
+		heap_t* get_relative_base(void);
+		void garbage_collect(void);
+		void mem_steal(uint16_t length);
+		void mem_unsteal(uint16_t length);
+		void delete_heap(void);
+		// Поля
 		heap_t *heap;
 		uint16_t base;
 		uint16_t length;
-	public:
-		Heap(int _length);
-		uint16_t get_base(void);
-		heap_t* get_heap(void);
-		uint16_t get_length(void);
-		void heap_dmp(void);
-		heap_id_t alloc(uint16_t size);
-		void free(heap_id_t id);
-		void realloc(heap_id_t id, uint16_t size);
-		uint16_t get_length(heap_id_t id);
-		void* get_addr(heap_id_t id);
-		uint16_t get_free_size(void);
-		heap_t* get_relative_base(void);
-		void mem_steal(uint16_t length);
-		void mem_unsteal(uint16_t length);
 };
 
-#endif
+#endif // SCR_HEAP_H
